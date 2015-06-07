4faa8b9d19b2eb2b46b0d9d3f2da5837aa72e0d9
EnriqueElGato <wo.shi.wo@live.com>
2015-06-07 16:51:37 -0400

    Updated box around legend.

--- plot4.R
+++ plot4.R
@@ -54,7 +54,7 @@ plot4 <- function(dataDir = "./"){
          plot (date_time2,Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
          lines(date_time2,data$Sub_metering_2, col = "red")
          lines(date_time2,data$Sub_metering_3, col = "blue")    
-         legend("topright", legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), col = c("black","red","blue"),  lwd=2)
+         legend("topright",bty="n", legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), col = c("black","red","blue"),  lwd=2)
          
          plot(date_time2,Global_reactive_power,type="l",xlab="datetime")          
     })
